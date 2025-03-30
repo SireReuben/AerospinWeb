7e06e272e88bf21cb085c019d3dbfd12bad66d1b
Reuben <74955311+SireReuben@users.noreply.github.com>
2025-03-31 01:04:51 +0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -79,7 +79,7 @@ HTML_CONTENT = '''
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aerospin Control Center</title>
-    <link href="s://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
